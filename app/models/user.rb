021eec4edd25e77f0626f5410770afb035803d16
Benson Kalahar <bensonk42@gmail.com>
2011-03-25 17:31:36 -0500

    fixed it so nicknames are assigned properly

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   has_many :api_keys
   has_many :authorizations
   def self.create_from_hash!(hash)
-    create(:name => hash['user_info']['name'], :nick => hash['user_info']['nick'])
+    create(:name => hash['user_info']['name'], :nick => hash['user_info']['nickname'])
   end
 end
