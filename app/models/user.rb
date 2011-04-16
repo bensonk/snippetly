4e1813e1eab5093a2d25a908b5083da9349d7870
Benson Kalahar <bensonk42@gmail.com>
2011-04-15 19:25:34 -0500

    Added relation for User.snippets, fixed <pre> tag

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_many :api_keys
   has_many :authorizations
+  has_many :snippets, :foreign_key => 'owner_id'
   def self.create_from_hash!(hash)
     create(:name => hash['user_info']['name'], :nick => hash['user_info']['nickname'])
   end
